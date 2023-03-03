ee450e8f33757f9933d4454100866327adb3e783
ikatainen <i.katainen@lancaster.ac.uk>
2023-03-03 13:40:17 +0000

    Update start.py

--- start.py
+++ start.py
@@ -9,6 +9,12 @@ M = 5.683*pow(10, 26)           #mass of Saturn with units [kg]
 g = 10.44                       #gravity ofSaturn with units [m/s^2]
 G = 6.674*pow(10, -11)          #gravitational constant with units [m^3 kg^-1 s^-2]
 
+# Defining velocity, position and mass
+ def Rings(, x, m):
+    self.v = v
+    self.x = x
+    self.m = m
+
 #Keplers third law for period T
 T = ((4* math.pi**2)/(G*M)*r**3)**(0.5)
 
