ac162059fed5aa950f605f4c1b9fc4e0894e7bb7
ikatainen <i.katainen@lancaster.ac.uk>
2023-03-06 10:25:50 +0000

    Update start.py

--- start.py
+++ start.py
@@ -1,25 +1,22 @@
 import math 
 import numpy as np
 import matplotlib.pyplot as plt 
+import random 
  
 
-#Useful values
+# Might be useful values
 r = 58232*pow(10, 3)            #radius of Saturn with units [m]
 M = 5.683*pow(10, 26)           #mass of Saturn with units [kg]
 g = 10.44                       #gravity ofSaturn with units [m/s^2]
 G = 6.674*pow(10, -11)          #gravitational constant with units [m^3 kg^-1 s^-2]
 
-# Defining velocity, position and mass
- def Rings(, x, m):
-    self.v = v
-    self.x = x
-    self.m = m
+# Generating random particle in a box 
+x = [random.randint(1, 50) for n in range(200)]
+y = [random.randint(1, 50) for n in range(200)]
 
-#Keplers third law for period T
-T = ((4* math.pi**2)/(G*M)*r**3)**(0.5)
 
-#Plotting a graph to show the points 
 
-plt.scatter(r, M)
 
+plt.scatter(x,y, color = 'red')
 plt.show()
+
