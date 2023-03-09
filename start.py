7597f40d25d63883ba50d0ef717a28cdb1c22912
ikatainen <i.katainen@lancaster.ac.uk>
2023-03-09 11:46:32 +0000

    Update start.py

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import random
 from matplotlib import animation 
  
 
-# Might be useful values
+# Useful values
 r = 58232*pow(10, 3)            # radius of Saturn with units [m]
 M = 5.683*pow(10, 26)           # mass of Saturn with units [kg]
 g = 10.44                       # gravity ofSaturn with units [m/s^2]
@@ -16,8 +16,8 @@ V = 4/3 * math.pi * pow(r_d, 3) # Volume of particles
 m_p = rho / V                   # Mass of paricles 
 
 # Generating random particle in a plot
-x = [random.uniform(-100, 100) for n in range(100)]
-y = [random.uniform(-100, 100) for n in range(100)]
+x = [random.uniform(-100, 100) for n in range(50)]
+y = [random.uniform(-100, 100) for n in range(50)]
 
 # Kepler's third law
 T = math.sqrt( 4 * pow(math.pi, 2) / (G * M) * pow(r, 3))
@@ -33,22 +33,36 @@ for j in range(len(y)):
    v_x.append(v)
 
 # Defining dx and dy for the arrows 
-dx = np.ones_like(x)
+dx = np.zeros_like(x)
 dy = np.zeros_like(y)
+
+def arrows(dx, x, y):
+    for i in range(len(y)):
+        if y[i] < 0:
+            dx[i] = 1
+        elif y[i] > 0:
+            dx[i] = -1
+    return dx
 
 # Plot the points and arrows
 fig, ax = plt.subplots()
 
 points, = ax.plot(x, y, 'ro')
 
+# Moonlet 
+plt.scatter(0, 0, color = 'black', s = 100)
+
+# Plotting the initail point 
 plt.scatter(x, y, color = 'red')
+
 # Plotting the arrows
-plt.quiver(x, y, dx, dy, scale =10)
+dx = arrows(dx, x, y)
+plt.quiver(x, y, dx, dy, scale = 10)
 
 # Making the points move
-for i in range(1000):
+for i in range(10000):
     x = [x[i] + v_x[i] for i in range(len(x))]
     points.set_data(x, y)
-    plt.pause(0.01)
+    plt.pause(0.001)
 
 plt.show()
