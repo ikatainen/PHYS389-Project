6c730eccfb7ae4e14dbc7c3b4f128c63341599e8
ikatainen <i.katainen@lancaster.ac.uk>
2023-03-28 14:53:58 -0500

    Update kineticenergy.py

--- kineticenergy.py
+++ kineticenergy.py
@@ -5,15 +5,14 @@ import random
 from matplotlib import animation 
 
 # Useful values
-
 r = 58232*pow(10, 3)            # radius of Saturn with units [m]
 M = 5.683*pow(10, 26)           # mass of Saturn with units [kg]
 g = 10.44                       # gravity ofSaturn with units [m/s^2]
 G = 6.674*pow(10, -11)          # gravitational constant with units [m^3 kg^-1 s^-2]
-rho = 1000                      # density of particle in Saturn's rings with units [kg/m^3]
-r_d = 500 * pow(10, -9)         # Average radius of particles with units [m]
+rho = 100                       # density of particle in Saturn's rings with units [kg/m^3]
+r_d = 0.06                      # Average radius of particles with units [m]
 V = 4/3 * math.pi * pow(r_d, 3) # Volume of particles 
-m_p = rho / V                   # Mass of paricles 
+m_p = 1e-16 #rho / V                   # Mass of paricles 
 
 # Generating random particle in a plot
 x = [random.uniform(-100, 100) for n in range(50)]
@@ -28,25 +27,31 @@ Omega = 2 * math.pi / T
 # Velocity in the x-direction
 v_x = np.array([-2/3 * Omega * y_i for y_i in y])
 
-# Definig a list for kinetic energy
-KE = []
+# Defining initial kinetic energy
+KE_0 = 0.5 * m_p * np.sum(np.power(v_x, 2))
 
-for i in range(len(v_x)):
-    kineticenergy = 0.5 * np.power(v_x[i], 2)
-    KE.append(kineticenergy)
+# Defining lists for kinetic energy and time
+KE = [KE_0]
+time = [0]
 
-# Definig a list for time
-timepos = []
-deltaT = 2000
+# Loop for calculating kinetic energy and time over 1 hour
+dt = 10 # time step in seconds
+for t in range(1, 3600, dt):
+    # Update particle positions and velocities
+    x += v_x * dt
+    y += np.zeros_like(y)
+    v_x += np.zeros_like(v_x)
+    time.append(t)
+    
+    # Calculate kinetic energy
+    KE_t = 0.5 * m_p * np.sum(np.power(v_x, 2))
+    KE.append(KE_t)
+    
 
-for i in range(len(v_x)):
-    timestep = deltaT * i
-    timepos.append(timestep)
-
-# Plotting a graph showing kinetic energy over a period of time 
-plt.plot(timepos, KE, label='Kinetic energy') 
+# Plotting a graph showing conservation of kinetic energy over time
+plt.figure(figsize=(6, 4))
+plt.plot(time, KE, color='purple') 
 plt.ylabel('Kinetic Energy (J)')
 plt.xlabel('Time (s)')
-plt.ylim([-1, 1])
-plt.legend()
+
 plt.show()
